7821f9c7672faa02d913bea02a8fa3af3cd97ef9
Oliver Soeser <o.soeser@icloud.com>
2024-07-08 12:22:20 +0100

    Initial commit

---
+++ build/index.js
@@ -0,0 +1,140 @@
+var dt = 1 / 50;
+var Vector2D = /** @class */ (function () {
+    function Vector2D(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    Vector2D.prototype.add = function (other) {
+        return new Vector2D(this.x + other.x, this.y + other.y);
+    };
+    Vector2D.prototype.to = function (other) {
+        return new Vector2D(other.x - this.x, other.y - this.y);
+    };
+    Vector2D.prototype.size = function () {
+        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+    };
+    Vector2D.prototype.times = function (factor) {
+        return new Vector2D(this.x * factor, this.y * factor);
+    };
+    return Vector2D;
+}());
+var Particle = /** @class */ (function () {
+    function Particle(pos, mass, radius, color) {
+        this.pos = pos;
+        this.velocity = new Vector2D(0, 0);
+        this.mass = mass;
+        this.charge = 6;
+        this.radius = radius;
+        this.color = color;
+    }
+    Particle.prototype.applyForce = function (force) {
+        var acceleration = force.times(1 / this.mass);
+        this.velocity = this.velocity.add(acceleration.times(dt));
+    };
+    Particle.prototype.step = function () {
+        this.pos = this.pos.add(this.velocity.times(dt));
+    };
+    return Particle;
+}());
+var Spring = /** @class */ (function () {
+    function Spring(source, target, color) {
+        this.source = source;
+        this.target = target;
+        this.ideal = 30;
+        this.stiffness = 2;
+        this.color = color;
+    }
+    return Spring;
+}());
+var Drawing = /** @class */ (function () {
+    function Drawing() {
+        this.canvas = document.getElementById("frame");
+        this.context = this.canvas.getContext("2d");
+    }
+    Drawing.prototype.drawParticle = function (particle) {
+        var pos = particle.pos;
+        this.context.strokeStyle = particle.color;
+        this.context.beginPath();
+        this.context.arc(pos.x, pos.y, particle.radius, 0, 2 * Math.PI);
+        this.context.stroke();
+    };
+    Drawing.prototype.drawSpring = function (spring) {
+        var start = spring.source.pos;
+        var end = spring.target.pos;
+        this.context.strokeStyle = spring.color;
+        this.context.moveTo(start.x, start.y);
+        this.context.lineTo(end.x, end.y);
+        this.context.stroke();
+    };
+    Drawing.prototype.clear = function () {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    };
+    return Drawing;
+}());
+var Physics = /** @class */ (function () {
+    function Physics() {
+        var _this = this;
+        this.G = 1.7 * Math.pow(10, 4);
+        this.ke = Math.pow(10, 4);
+        this.drawing = new Drawing();
+        this.particle_array = new Array();
+        this.spring_array = new Array();
+        for (var i = 0; i < 100; i++) {
+            this.particle_array.push(new Particle(new Vector2D(150 + Math.random() * 700, 150 + Math.random() * 700), 10, 10, "black"));
+        }
+        for (var i = 0; i < 40; i++) {
+            this.spring_array.push(new Spring(this.particle_array[i], this.particle_array[i + 1], "#cedfe8"));
+        }
+        this.particle_array.forEach(function (particle) {
+            _this.drawing.drawParticle(particle);
+        });
+        this.spring_array.forEach(function (spring) {
+            _this.drawing.drawSpring(spring);
+        });
+        setInterval(this.step.bind(this), 1000 * dt);
+    }
+    Physics.prototype.step = function () {
+        var _this = this;
+        this.particle_array.forEach(function (particle) {
+            // Gravity
+            var centre = new Particle(new Vector2D(500, 500), 100, 10, "black");
+            particle.applyForce(_this.gravitationalForce(particle, centre));
+            _this.particle_array.forEach(function (other) {
+                // Charge
+                particle.applyForce(_this.electricForce(particle, other));
+            });
+        });
+        this.spring_array.forEach(function (spring) {
+            var r_vec = spring.source.pos.to(spring.target.pos);
+            var r = Math.max(r_vec.size(), spring.source.radius);
+            var ideal_vec = r_vec.times(spring.ideal / r);
+            var force = r_vec.add(ideal_vec.times(-1)).times(spring.stiffness);
+            spring.source.applyForce(force);
+            spring.target.applyForce(force.times(-1));
+        });
+        this.drawing.clear();
+        this.particle_array.forEach(function (particle) {
+            particle.step();
+            _this.drawing.drawParticle(particle);
+        });
+        this.spring_array.forEach(function (spring) {
+            _this.drawing.drawSpring(spring);
+        });
+    };
+    Physics.prototype.gravitationalForce = function (p1, p2) {
+        var r_vec = p1.pos.to(p2.pos);
+        var r = Math.max(r_vec.size(), p1.radius);
+        var r_uvec = r_vec.times(1 / r);
+        var force = r_uvec.times((this.G * p1.mass * p2.mass) / (Math.pow(r, 2)));
+        return force;
+    };
+    Physics.prototype.electricForce = function (p1, p2) {
+        var r_vec = p2.pos.to(p1.pos);
+        var r = Math.max(r_vec.size(), p1.radius);
+        var r_uvec = r_vec.times(1 / r);
+        var force = r_uvec.times((this.ke * p1.charge * p2.charge) / (Math.pow(r, 2)));
+        return force;
+    };
+    return Physics;
+}());
+new Physics();
---
+++ index.ts
@@ -0,0 +1,191 @@
+const dt = 1/50;
+
+class Vector2D {
+    public x: number;
+    public y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    add(other: Vector2D): Vector2D {
+        return new Vector2D(this.x + other.x, this.y + other.y);
+    }
+
+    to(other: Vector2D): Vector2D {
+        return new Vector2D(other.x - this.x, other.y - this.y);
+    }
+
+    size(): number {
+        return Math.sqrt(this.x**2 + this.y**2);
+    }
+
+    times(factor: number): Vector2D {
+        return new Vector2D(this.x * factor, this.y * factor);
+    }
+}
+
+class Particle {
+    public pos: Vector2D;
+    public velocity: Vector2D;
+    public mass: number;
+    public charge: number;
+    public radius: number;
+    public color: string;
+
+    constructor(pos: Vector2D, mass: number, radius: number, color: string) {
+        this.pos = pos;
+        this.velocity = new Vector2D(0, 0);
+        this.mass = mass;
+        this.charge = 6;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    applyForce(force: Vector2D) {
+        let acceleration = force.times(1/this.mass);
+        this.velocity = this.velocity.add(acceleration.times(dt));
+    }
+
+    step() {
+        this.pos = this.pos.add(this.velocity.times(dt));
+    }
+}
+
+class Spring {
+    public source: Particle;
+    public target: Particle;
+    public ideal: number;
+    public stiffness: number;
+    public color: string;
+
+    constructor(source: Particle, target: Particle, color: string) {
+        this.source = source;
+        this.target = target;
+        this.ideal = 30;
+        this.stiffness = 2;
+        this.color = color;
+    }
+}
+
+class Drawing {
+    private canvas: HTMLCanvasElement;
+    private context: CanvasRenderingContext2D;
+
+    constructor() {
+        this.canvas = document.getElementById("frame") as HTMLCanvasElement;
+        this.context = this.canvas.getContext("2d");
+    }
+
+    drawParticle(particle: Particle) {
+        let pos = particle.pos;
+
+        this.context.strokeStyle = particle.color;
+
+        this.context.beginPath();
+        this.context.arc(pos.x, pos.y, particle.radius, 0, 2 * Math.PI);
+        this.context.stroke();
+    }
+
+    drawSpring(spring: Spring) {
+        let start = spring.source.pos;
+        let end = spring.target.pos;
+
+        this.context.strokeStyle = spring.color;
+
+        this.context.moveTo(start.x, start.y);
+        this.context.lineTo(end.x, end.y);
+        this.context.stroke();
+    }
+
+    clear() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
+
+class Physics {
+    private drawing: Drawing;
+    private G: number = 1.7*10**4;
+    private ke: number = 10**4;
+
+    private particle_array: Particle[];
+    private spring_array: Spring[];    
+
+    constructor() {
+        this.drawing = new Drawing();
+
+        this.particle_array = new Array();
+        this.spring_array = new Array();
+
+        for (let i = 0; i < 100; i++) {
+            this.particle_array.push(new Particle(new Vector2D(150 + Math.random()*700, 150 + Math.random()*700), 10, 10, "black"));
+        }
+
+        for (let i = 0; i < 40; i++) {
+            this.spring_array.push(new Spring(this.particle_array[i], this.particle_array[i+1], "#cedfe8"));
+        }
+
+        this.particle_array.forEach(particle => {
+            this.drawing.drawParticle(particle);
+        });
+
+        this.spring_array.forEach(spring => {
+            this.drawing.drawSpring(spring);
+        });
+
+        setInterval(this.step.bind(this), 1000*dt);
+    }
+
+    step() {
+        this.particle_array.forEach(particle => {
+            // Gravity
+            let centre = new Particle(new Vector2D(500, 500), 100, 10, "black")
+
+            particle.applyForce(this.gravitationalForce(particle, centre));
+
+            this.particle_array.forEach(other => {
+                // Charge
+                particle.applyForce(this.electricForce(particle, other));
+            });
+        });
+
+        this.spring_array.forEach(spring => {
+            let r_vec = spring.source.pos.to(spring.target.pos);
+            let r = Math.max(r_vec.size(), spring.source.radius);
+            let ideal_vec = r_vec.times(spring.ideal/r);
+            let force = r_vec.add(ideal_vec.times(-1)).times(spring.stiffness);
+            spring.source.applyForce(force);
+            spring.target.applyForce(force.times(-1));
+        });
+
+        this.drawing.clear();
+
+        this.particle_array.forEach(particle => {
+            particle.step();
+            this.drawing.drawParticle(particle);
+        });
+
+        this.spring_array.forEach(spring => {
+            this.drawing.drawSpring(spring);
+        });
+    }
+
+    gravitationalForce(p1: Particle, p2: Particle): Vector2D {
+        let r_vec = p1.pos.to(p2.pos);
+        let r = Math.max(r_vec.size(), p1.radius);
+        let r_uvec = r_vec.times(1/r);
+        let force = r_uvec.times((this.G * p1.mass * p2.mass) / (r**2));
+        return force
+    }
+
+    electricForce(p1: Particle, p2: Particle): Vector2D {
+        let r_vec = p2.pos.to(p1.pos);
+        let r = Math.max(r_vec.size(), p1.radius);
+        let r_uvec = r_vec.times(1/r);
+        let force = r_uvec.times((this.ke * p1.charge * p2.charge)/(r**2));
+        return force;
+    }
+}
+
+new Physics();
