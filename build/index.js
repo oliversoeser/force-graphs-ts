8737e713d31173928e366e63cd3816ccc06d76ce
Oliver Soeser <o.soeser@icloud.com>
2024-07-15 10:57:48 +0100

    Revised File Structure

--- build/index.js
+++ build/index.js
@@ -1,29 +1,38 @@
 var dt = 1 / 50;
-var Vector2D = (function () {
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+var Vector2D = /** @class */ (function () {
     function Vector2D(x, y) {
         this.x = x;
         this.y = y;
     }
+    // The sum of the vectors
     Vector2D.prototype.add = function (other) {
         return new Vector2D(this.x + other.x, this.y + other.y);
     };
+    // The vector from itself to the other vector
     Vector2D.prototype.to = function (other) {
         return new Vector2D(other.x - this.x, other.y - this.y);
     };
+    // The magnitude of the vector
     Vector2D.prototype.size = function () {
         return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
     };
+    // The vector multiplied by a factor
     Vector2D.prototype.times = function (factor) {
         return new Vector2D(this.x * factor, this.y * factor);
     };
     return Vector2D;
 }());
-var Particle = (function () {
+var Particle = /** @class */ (function () {
     function Particle(pos, mass, radius, color) {
         this.pos = pos;
         this.velocity = new Vector2D(0, 0);
         this.mass = mass;
-        this.charge = 14;
+        this.charge = 1;
         this.force = new Vector2D(0, 0);
         this.radius = radius;
         this.color = color;
@@ -32,24 +41,23 @@ var Particle = (function () {
         this.force = this.force.add(force);
     };
     Particle.prototype.step = function () {
-        var acceleration = this.force.times(1 / this.mass);
-        this.velocity = this.velocity.add(acceleration.times(dt));
-        this.pos = this.pos.add(this.velocity.times(dt));
+        this.velocity = this.force.times(1 / this.mass);
+        this.pos = this.pos.add(this.velocity.times(3));
         this.force = new Vector2D(0, 0);
     };
     return Particle;
 }());
-var Spring = (function () {
+var Spring = /** @class */ (function () {
     function Spring(source, target, color) {
         this.source = source;
         this.target = target;
-        this.ideal = 30;
-        this.stiffness = 5;
+        this.ideal = 20;
+        this.stiffness = 2;
         this.color = color;
     }
     return Spring;
 }());
-var Mouse = (function () {
+var Mouse = /** @class */ (function () {
     function Mouse() {
         this.down = false;
         this.pos = new Vector2D(0, 0);
@@ -57,7 +65,7 @@ var Mouse = (function () {
     }
     return Mouse;
 }());
-var Drawing = (function () {
+var Drawing = /** @class */ (function () {
     function Drawing() {
         var _this = this;
         this.canvas = document.getElementById("frame");
@@ -93,19 +101,19 @@ var Drawing = (function () {
     };
     return Drawing;
 }());
-var Physics = (function () {
+var Physics = /** @class */ (function () {
     function Physics() {
         var _this = this;
-        this.G = 1.7 * Math.pow(10, 4);
-        this.ke = Math.pow(10, 4);
+        // Physical constants
+        this.ke = 1;
         this.drawing = new Drawing();
         this.particle_array = new Array();
         this.spring_array = new Array();
         for (var i = 0; i < 100; i++) {
             this.particle_array.push(new Particle(new Vector2D(150 + Math.random() * 700, 150 + Math.random() * 700), 10, 10, "black"));
         }
-        for (var i = 0; i < 40; i++) {
-            this.spring_array.push(new Spring(this.particle_array[i], this.particle_array[i + 1], "#cedfe8"));
+        for (var i = 0; i < 70; i++) {
+            this.spring_array.push(new Spring(this.particle_array[i], this.particle_array[getRandomInt(0, 99)], "#cedfe8"));
         }
         this.particle_array.forEach(function (particle) {
             _this.drawing.drawParticle(particle);
@@ -119,19 +127,30 @@ var Physics = (function () {
         var _this = this;
         this.particle_array.forEach(function (particle) {
             _this.particle_array.forEach(function (other) {
-                particle.applyForce(_this.gravitationalForce(particle, other));
-                particle.applyForce(_this.electricForce(particle, other));
+                // Charge
+                var adjacent = false;
+                _this.spring_array.forEach(function (spring) {
+                    if ((spring.source == particle && spring.target == other) || (spring.source == other && spring.target == particle)) {
+                        adjacent = true;
+                    }
+                });
+                if (!adjacent) {
+                    particle.applyForce(_this.electricForce(particle, other));
+                }
             });
         });
+        // Spring forces
         this.spring_array.forEach(function (spring) {
             var r_vec = spring.source.pos.to(spring.target.pos);
-            var r = Math.max(r_vec.size(), spring.source.radius);
-            var ideal_vec = r_vec.times(spring.ideal / r);
-            var force = r_vec.add(ideal_vec.times(-1)).times(spring.stiffness);
+            var d = Math.max(r_vec.size(), spring.source.radius);
+            var r_uvec = r_vec.times(1 / d); // Unit vector
+            var force = r_uvec.times(spring.stiffness * Math.log(d / spring.ideal));
             spring.source.applyForce(force);
             spring.target.applyForce(force.times(-1));
         });
+        // Reset canvas
         this.drawing.clear();
+        // Draw
         this.particle_array.forEach(function (particle) {
             particle.step();
             _this.drawing.drawParticle(particle);
@@ -140,18 +159,11 @@ var Physics = (function () {
             _this.drawing.drawSpring(spring);
         });
     };
-    Physics.prototype.gravitationalForce = function (p1, p2) {
-        var r_vec = p1.pos.to(p2.pos);
-        var r = Math.max(r_vec.size(), p1.radius + p2.radius);
-        var r_uvec = r_vec.times(1 / r);
-        var force = r_uvec.times((this.G * p1.mass * p2.mass) / (Math.pow(r, 2)));
-        return force;
-    };
     Physics.prototype.electricForce = function (p1, p2) {
         var r_vec = p2.pos.to(p1.pos);
-        var r = Math.max(r_vec.size(), p1.radius + p2.radius);
-        var r_uvec = r_vec.times(1 / r);
-        var force = r_uvec.times((this.ke * p1.charge * p2.charge) / (Math.pow(r, 2)));
+        var d = Math.max(r_vec.size(), p1.radius + p2.radius);
+        var r_uvec = r_vec.times(1 / d); // Unit vector
+        var force = r_uvec.times(this.ke / Math.sqrt(d));
         return force;
     };
     return Physics;
